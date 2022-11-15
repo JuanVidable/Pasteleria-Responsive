85c1ea0fbef11038549eb94a63c9e869a20f343b
JuanVidable <61946715+JuanVidable@users.noreply.github.com>
2022-11-15 00:15:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// test
+const btn = document.getElementById("btn");
+const menu = document.querySelector('.nav__menu');
+const acerca = document.getElementById("acerca");
+const servicios = document.getElementById("servicios");
+
+
+btn.addEventListener('click', function(){
+    menu.classList.toggle("show");
+})
+
+acerca.addEventListener('click', function(){
+    window.scrollTo(0,2000);
+    menu.classList.toggle("show");
+    
+})
+
+servicios.addEventListener('click', function(){
+    if(window.screen.width >= 1400){
+        window.scrollTo(0,850);
+    }else if(window.screen.width>= 980){
+        window.scrollTo(0,750);
+    }else if(window.screen.width>300){
+        window.scrollTo(0,500)
+    }
+    if(window.screen.width<=800){
+        menu.classList.toggle("show");
+    }
+    
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+});
+
+
+
+
